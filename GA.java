55104d18ecb78b1f57fa9de3384796e8604c3818
Tom Lucy <tlucy@bowdoin.edu>
2018-12-15 22:17:40 -0500

    ga commented

--- GA.java
+++ GA.java
@@ -1,14 +1,34 @@
+/*
+ * Author Luca Ostertag-Hill, Tom Lucy, Jake Rourke
+ * Date 12/16/2018
+ * 
+ * This class defines the Genetic Algorithm. Called by SMTWTP_HYBRID and given
+ * the user inputed values, it runs the GA for the specified number of
+ * generations. If being run as a part of the hybrid, the GA algorithm 
+ * performs local search on a population of solutions produced by the EAS
+ * algorithm. If being run alone, GA runs on a randomly initialized set of
+ * possible workflows. In each generation, the tournament selection is
+ * performed to select a breeding pool, offspring are generated through
+ * Order One Crossover, and two types of mutation, General Swap and Range
+ * Reversal, may be performed on the new population. 
+ * 
+ */
+
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
 
 public class GA {
 		
+	// the number of workflows in the population
     public int population_size;
+    // the probability of mutation on a given individual
     public double mutation_prob;
+    // the number of generations
     public int max_generations;
+    // the number of jobs in a workflow
     public int num_jobs;
-
+    // the probability of crossover on a given set of parents
     public double crossover_prob;
 
     public INDIVIDUAL[] population;
@@ -23,6 +43,13 @@ public class GA {
     	this.crossover_prob = crossover_prob;
     }
 
+	/*
+	 * Purpose: The main algorithm of the GA. It iteratively selects a breeding pool,
+	 * performs crossover, and mutates offspring.
+	 * Parameters: A 2d integer array of initial workflows, the smtwtp problem object
+	 * Return: none, prints the best so far in each iteration
+	 * 
+	 */
 	public void RunGA(int[][] workflows, SMTWTP smtwtp) {
 		
 		Random rand = new Random();
@@ -36,6 +63,7 @@ public class GA {
 		this.population_size = workflows.length;	
 		population = new INDIVIDUAL[population_size];
 		
+		// create the initial individuals
 		for (int i = 0; i < population_size; i++) {
 			population[i] = new INDIVIDUAL(num_jobs, smtwtp, workflows[i]);
 			population[i].scoreWorkflow();
@@ -44,6 +72,7 @@ public class GA {
 		int best_score = Integer.MAX_VALUE;
 		int[] best_workflow = new int[num_jobs];
 		
+		// find the best member of the inital populaiton
 		for (int i = 0; i < population_size; i++) {
 			if (population[i].getWorkflow_score() < best_score) {
 				best_score = population[i].getWorkflow_score();
@@ -55,6 +84,7 @@ public class GA {
 		
 		generation = 0;
 		
+		// for each generation
 		while (generation < max_generations) {
 			
 			new_population = new INDIVIDUAL[population_size];
@@ -63,16 +93,19 @@ public class GA {
 					
 			while (count < population_size) {
 						
+				// select parents for breeding
 				parents = tournamentSelection(population);
 						
+				// perform crossover with some probability
 				if (rand.nextDouble() < crossover_prob) {
-					children = iPOX(parents);
+					children = orderOneCrossover(parents);
 				}
 				else {
 					children[0] = parents[0];
 					children[1] = parents[1];
 				}
 						
+				// add children to the new population
 				new_population[count] = children[0];
 				++count;
 				
@@ -82,8 +115,10 @@ public class GA {
 				}
 			}
 			
+			// mutate the new population
 			population = mutation(new_population);
 			
+			// find the best workflow so far
 			for (int i = 0; i < population_size; i++) {
 				population[i].scoreWorkflow();
 				if (population[i].getWorkflow_score() < best_score) {
@@ -92,15 +127,18 @@ public class GA {
 				}
 			}
 			
-
-			//System.out.println("Generation " + generation + ", best score is " + best_score);
-			
 			generation++;
 		}
 		System.out.println(best_score);
 	}
     
-    // return two individuals that have undergone tournament selection
+	/*
+	 * Purpose: This function takes in the population and performs tournament selection
+	 * twice to select two parents for breeding. 
+	 * Parameters: An array of all individuals in the population
+	 * Return: An array of two parents
+	 * 
+	 */
     public INDIVIDUAL[] tournamentSelection(INDIVIDUAL[] population) {
 
         Random rand = new Random();
@@ -133,7 +171,16 @@ public class GA {
         return parents;
     }
     
-    public INDIVIDUAL[] iPOX(INDIVIDUAL[] parents) {
+	/*
+	 * Purpose: Performs crossover on two parents in an order one fashion. To create an
+	 * offspring, a substring from the beginning of a parent is selected. The remaining
+	 * slots are filled by iterating through the other parent and inserting a symbol if
+	 * it was not already included in the offspring.
+	 * Parameters: An array of two parents
+	 * Return: An array of two offspring
+	 * 
+	 */
+    public INDIVIDUAL[] orderOneCrossover(INDIVIDUAL[] parents) {
     	
     	Random rand = new Random();
 		Set<Integer> child1_set = new HashSet<Integer>();
@@ -150,6 +197,7 @@ public class GA {
     	int[] workflow1 = parents[0].getWorkflow();
     	int[] workflow2 = parents[1].getWorkflow();
     	
+    	// fill the beginning of the children up to the cut point
     	for (int i = 0; i < cut_point; i++) {
     		child1_workflow[i] = workflow1[i];
     		child2_workflow[i] = workflow2[i];
@@ -161,6 +209,7 @@ public class GA {
     	curr = cut_point;
     	index = 0;
     	
+    	// fill the remainder of child 1
     	while (curr < num_jobs && index < num_jobs) {
     		
     		if (!child1_set.contains(workflow2[index])) {
@@ -175,6 +224,7 @@ public class GA {
     	curr = cut_point;
     	index = 0;
     	
+    	// fill the remainder of child 2
     	while (curr < num_jobs && index < num_jobs) {
     		
     		if (!child2_set.contains(workflow1[index])) {
@@ -192,7 +242,15 @@ public class GA {
     	return children;
     }
     
-    // potentially mutate each characteristic for each individual in population
+    /*
+	 * Purpose: With some probability, performs one or both General Swap and
+	 * Range Reversal mutation. General Swap mutation selects two indices
+	 * in the workflow and swaps their contents. Range Reversal selects a 
+	 * random range of length 2 to 4 in the workflow and reverses the contents.
+	 * Parameters: The entire population
+	 * Return: The entire population, with some mutation performed
+	 * 
+	 */
     public INDIVIDUAL[] mutation(INDIVIDUAL[] population){
     	
         Random rand = new Random();
@@ -212,6 +270,7 @@ public class GA {
         		int starting_index = rand.nextInt(num_jobs - range_size);
         		int counter = range_size - 1;
         	
+        		// reverse the order of only the selected range
         		for (int j = starting_index; j < starting_index + (range_size / 2); j++) {
         			temp = workflow[j];
         			workflow[j] = workflow[j + counter];
